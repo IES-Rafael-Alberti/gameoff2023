5033a0056b2f9c6b5d29ffba2bd6ac886fbb827b
fjortegan <fjortegan@gmail.com>
2023-11-15 13:07:30 +0100

    events testing

---
+++ Assets/ChapterManager/EventManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager {
+    public UnityEvent NextChapter = new UnityEvent();
+    public UnityEvent GameOver = new UnityEvent();
+    public UnityEvent EndGame = new UnityEvent();
+    
+    public void InvokeNextChapter() {
+        NextChapter?.Invoke();
+    }
+}
