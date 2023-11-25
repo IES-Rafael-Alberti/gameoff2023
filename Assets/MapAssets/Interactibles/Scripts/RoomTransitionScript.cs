78f89a00c725cd4c55d7d0b84441b5ec34c5796d
BoofWoof <glfosdick@charter.net>
2023-11-24 23:37:33 -0800

    Updated room transition to fix glitch.

--- Assets/MapAssets/Interactibles/Scripts/RoomTransitionScript.cs
+++ Assets/MapAssets/Interactibles/Scripts/RoomTransitionScript.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.ShaderGraph;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
@@ -8,19 +9,32 @@ using static UnityEngine.GraphicsBuffer;
 public class RoomTransitionScript : MonoBehaviour
 {
     public string direction;
-    private void OnTriggerEnter(Collider other)
+    private static bool enabled = true;
+    GameObject target;
+
+    private void Update()
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (enabled && target != null)
         {
-            GameObject target = other.gameObject;
-            Vector3 warpPosition = transform.position + transform.right * 9f;
+            enabled = false;
+            StartCoroutine(Renable());
+
+            float shift_magnitude = 9f;
+            if (Mathf.Abs(transform.right.y) > Mathf.Abs(transform.right.x))
+            {
+                shift_magnitude = 15f;
+            }
+
+            Vector3 warpPosition = transform.position + transform.right * shift_magnitude;
+
             target.transform.position = new Vector3(warpPosition.x, target.transform.position.y, warpPosition.z);
 
             CubeScript parentData = transform.parent.GetComponent<CubeScript>();
             Vector2Int currentPosition = parentData.position;
             GameObject[,] map = parentData.source_board.board_data.map_objects;
             GameObject next_room;
-            switch (direction){
+            switch (direction)
+            {
                 case "Up":
                     next_room = map[currentPosition.x - 1, currentPosition.y];
                     CameraTrackingScript.targetRoom = next_room;
@@ -45,5 +59,26 @@ public class RoomTransitionScript : MonoBehaviour
                     break;
             }
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            target = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            target = null;
+        }
+    }
+    IEnumerator Renable()
+    {
+        yield return new WaitForSeconds(0.5f);
+        enabled = true;
+        yield break;
     }
 }
