7b7957f8a99d6121d33e9f46087c11e5916994f9
BoofWoof <glfosdick@charter.net>
2023-11-29 09:23:25 -0800

    Added menu controls.

--- Assets/Intro/Scripts/MoveIndicator.cs
+++ Assets/Intro/Scripts/MoveIndicator.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class MoveIndicator : MonoBehaviour, IPointerEnterHandler
+public class MoveIndicator : MonoBehaviour, IPointerEnterHandler, ISelectHandler
 {
     public GameObject Indicator;
 
     // Called when the mouse pointer enters the button
     public void OnPointerEnter(PointerEventData eventData)
+    {
+        Indicator.SetActive(true);
+        Indicator.transform.position = transform.position;
+    }
+    public void OnSelect(BaseEventData eventData)
     {
         Indicator.SetActive(true);
         Indicator.transform.position = transform.position;
